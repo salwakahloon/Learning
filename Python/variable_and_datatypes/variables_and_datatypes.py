8f0b3067bd09669964d19dd233202814df75b828
salwakahloon <salwabasharat08>
2024-08-24 14:37:54 +0500

    data types and variable in python 24-08-2024

---
+++ Python/variable_and_datatypes/variables_and_datatypes.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    # Variables with different datatype
+    name = "EhsasLab"
+    print(name)
+
+    name2:str = "Welcom to EhsasLab"
+    print(name2)
+    
+    a:int = 20
+    print(a)
+
+    b:float = 90.45
+    print(b)
+
+    ab:bool = True
+    ba:bool = False
+
+    print(ab)
+    print(ba)
+
+    # Get input from user
+
+    name:str = input("Please Enter your name: ")
+    print(name)
+
+    age:int = input("Please enter your age: ")
+    print(age)
+
