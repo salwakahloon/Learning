4b41861e5fb106efaa23af1d5809ba4661fe1478
salwakahloon <salwabasharat08>
2024-08-25 12:37:39 +0500

    variable and datatype practice

---
+++ Python/variable_and_datatypes/practice.py
@@ -0,0 +1,29 @@
+if __name__== "__main__":
+    # variables with different datatype
+    name = "DigitalNarowal"
+    print(name)
+    
+    name2:str = "welcome to digital narowal"
+    print(name2)
+
+    a:int = 30
+    print(a)
+
+    b:float = 92.25
+    print(b)
+
+    ab:bool = True
+    ba:bool = False
+    print(ab)
+    print(ba)
+
+    # Get input from user
+    name = input("Enter your name:")
+    print(name)
+
+    age:int = input("Enter your age:")
+    print(age)
+    
+
+
+
